554f818715279a4914620ae1726853dc1d8fc6ea
Salvador Gonzalez <gonzalezlsalvador@outlook.com>
2023-08-31 03:05:50 -0700

    Side Navigation Bar, Drinks Controller and HotCoffee View Created

---
+++ PP.CoffeeShop.Presentation/Controllers/DrinksController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PP.CoffeeShop.Presentation.Business.Services;
+
+namespace PP.CoffeeShop.Presentation.Controllers
+{
+    public class DrinksController : Controller
+    {
+        private readonly IDrinkService _drinkService;
+
+        public DrinksController(IDrinkService drinkService)
+        {
+            _drinkService = drinkService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HotCoffees(int drinkCategory)
+        {
+            var hotCoffees = _drinkService.GetDrinksByCategory(drinkCategory);
+            return View(hotCoffees);
+        }
+    }
+}
