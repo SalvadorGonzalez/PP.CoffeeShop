940aa6d072600419e5d6a6c6350a1839f02ca565
Salvador Gonzalez <gonzalezlsalvador@outlook.com>
2023-09-05 16:16:11 -0700

    Drinks Foreign Key updated

--- PP.CoffeeShop.Presentation/Migrations/20230905231042_WhatsChanged.Designer.cs
+++ PP.CoffeeShop.Presentation/Migrations/20230905231042_WhatsChanged.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PP.CoffeeShop.Presentation.Data;
 
@@ -10,9 +11,11 @@ using PP.CoffeeShop.Presentation.Data;
 namespace PP.CoffeeShop.Presentation.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230905231042_WhatsChanged")]
+    partial class WhatsChanged
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
